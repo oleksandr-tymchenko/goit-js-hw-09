8ec771aabeef656da6c7185b06ead5f9048f0c95
oleksandr-tymchenko <116674828+oleksandr-tymchenko@users.noreply.github.com>
2023-04-02 21:23:16 +0000

    Deploying to gh-pages from @ oleksandr-tymchenko/goit-js-hw-09@24f44a62e19b2c601e6cac6afe3f6aa8abb2aaeb 🚀

---
+++ 01-color-switcher.2b972e1f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),r=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){o=setInterval((()=>{t.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3),e.setAttribute("disabled",!0),r.removeAttribute("disabled")})),r.addEventListener("click",(function(){clearInterval(o),n()})),n();let o=null;function n(){e.removeAttribute("disabled"),r.setAttribute("disabled",!0)}
+//# sourceMappingURL=01-color-switcher.2b972e1f.js.map
